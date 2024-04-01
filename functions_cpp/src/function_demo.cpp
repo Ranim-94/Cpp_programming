9033169af3598c4518034255c7c8167239874632
Ranim Tom <ranim1tom@gmail.com>
2024-04-01 19:59:40 +0200

    Adding function lesson 1.
    This demo contains basic functions concetp in Cpp. Main points are:
    1- vocab: paramters refers to functions definition (or implementation), and arguments refers to functions calls (usually in main.cpp or some other source file)
    2- in Cpp, we could name 2 functions same name, the compiler will differentiate between them based on argument type, nb of arguments, return type,...

---
+++ functions_cpp/src/function_demo.cpp
@@ -0,0 +1,37 @@
+
+#include"function_demo.hpp"
+// #include<iostream>
+
+float add_nb(float nb1, float nb2){
+
+/*
+    - Here we have the defintion, or implementation of the function
+    - nb1 and nb2 are called parameters
+        (in main.c or whatever .c the function is called: they are called arguments)
+*/
+
+float result;
+
+result = nb1 + nb2 ;
+
+return result;
+
+} // End add_two_nb
+
+int add_nb(int nb1, int nb2){
+
+/*
+    - In Cpp, we could name 2 different functions with same name
+    - the compiler is able to distinguish between them in the call
+    by using for example parameters type, nb of parameters,...
+
+*/
+
+int result;
+
+result = nb1 + nb2 ;
+
+return result;
+
+
+} // End add_two_nb
---
+++ functions_cpp/src/function_demo.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// Declaring function prototype
+float add_nb(float nb1, float nb2);
+
+int add_nb(int nb1, int nb2);
